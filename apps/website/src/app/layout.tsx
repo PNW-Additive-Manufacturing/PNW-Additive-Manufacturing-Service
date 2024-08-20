5285a9286ad51cc7f6c428f5d5dc8aef729f5d58
Aaron J <aaronjung@me.com>
2024-08-19 20:07:24 -0500

    switch themeColor to meta tag

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -33,7 +33,6 @@ export async function generateMetadata(
 		icons: ["/assets/am_logo.png"],
 		applicationName: "Purdue Northwest Additive Manufacturing Service",
 		category: "3D Printing",
-		themeColor: "#b1810b",
 		openGraph: {
 			type: "website",
 			siteName: "PNW Additive Manufacturing Service",
@@ -66,6 +65,7 @@ export default async function RootLayout({
 					href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
 					rel="stylesheet"
 				/>
+				<meta name="theme-color" content="#b1810b" />
 				<meta
 					name="viewport"
 					content="width=device-width, initial-scale=1.0"></meta>
