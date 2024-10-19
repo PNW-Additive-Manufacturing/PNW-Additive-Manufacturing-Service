8ebc80f32af452d6f03346e3c3de34a67716e85b
Aaron J <aaronjung@me.com>
2024-10-19 14:09:29 -0500

    Update schedule

--- app/components/Schedule.tsx
+++ app/components/Schedule.tsx
@@ -9,28 +9,28 @@ export const weeklyScheduleUTC: WeeklySchedule = [
     null,
     // Monday
     {
-        open: Date.UTC(0, 0, 0, 18, 0, 0, 0), // 13:00 CDT + 5 hours = 18:00 UTC
-        close: Date.UTC(0, 0, 0, 22, 0, 0, 0) // 17:00 CDT + 5 hours = 22:00 UTC
+        open: Date.UTC(0, 0, 0, 18, 0, 0, 0),
+        close: Date.UTC(0, 0, 0, 22, 0, 0, 0)
     },
     // Tuesday
     {
-        open: Date.UTC(0, 0, 0, 17, 30, 0, 0), // 11:30 CDT + 5 hours = 16:30 UTC
-        close: Date.UTC(0, 0, 0, 21, 30, 0, 0) // 16:30 CDT + 5 hours = 21:30 UTC
+        open: Date.UTC(0, 0, 0, 17, 30, 0, 0),
+        close: Date.UTC(0, 0, 0, 22, 30, 0, 0)
     },
     // Wednesday
     {
-        open: Date.UTC(0, 0, 0, 20, 30, 0, 0), // 15:30 CDT + 5 hours = 20:30 UTC
-        close: Date.UTC(0, 0, 0, 21, 30, 0, 0) // 16:30 CDT + 5 hours = 21:30 UTC
+        open: Date.UTC(0, 0, 0, 20, 30, 0, 0),
+        close: Date.UTC(0, 0, 0, 22, 30, 0, 0)
     },
     // Thursday
     {
-        open: Date.UTC(0, 0, 0, 17, 0, 0, 0), // 12:00 CDT + 5 hours = 17:00 UTC
-        close: Date.UTC(0, 0, 0, 19, 0, 0, 0) // 14:00 CDT + 5 hours = 19:00 UTC
+        open: Date.UTC(0, 0, 0, 17, 0, 0, 0),
+        close: Date.UTC(0, 0, 0, 19, 0, 0, 0)
     },
     // Friday
     {
-        open: Date.UTC(0, 0, 0, 19, 0, 0, 0), // 14:00 CDT + 5 hours = 19:00 UTC
-        close: Date.UTC(0, 0, 0, 21, 0, 0, 0) // 16:00 CDT + 5 hours = 21:00 UTC
+        open: Date.UTC(0, 0, 0, 19, 0, 0, 0),
+        close: Date.UTC(0, 0, 0, 21, 0, 0, 0)
     },
     // Saturday
     null
