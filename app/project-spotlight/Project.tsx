37ef9c36df57b02261eca2203faeafacaf352522
Aaron J <aaronjung@me.com>
2024-12-05 21:01:35 -0600

    Expand projects shown in spotlight

--- app/project-spotlight/Project.tsx
+++ app/project-spotlight/Project.tsx
@@ -6,13 +6,12 @@ export function Project({ title, author, description, imageSrc }: { title: strin
 
     return <div className="bg-gray-50 out shadow-sm rounded-md">
 
-        {imageSrc && <Image className="bg-background shadow-sm rounded-b-none rounded-t-sm" style={{ aspectRatio: "auto 4/3" }} src={imageSrc} alt={"Image"} width={720} height={720}></Image>}
+        {imageSrc && <Image className="bg-background shadow-sm rounded-b-none rounded-t-sm" style={{ aspectRatio: "2/1" }} src={imageSrc} alt={"Image"} width={720} height={720}></Image>}
 
         <div className="p-5">
             <h2 className="font-medium text-lg">{title}</h2>
-            <p className="text-sm italic">- {author}</p>
-            <br />
-            {show ? <p>{description}</p> : <div>
+            <p className="text-sm italic mb-3">{author}</p>
+            {show ? <p className="text-sm">{description}</p> : <div className="text-sm">
 
                 <span>{description.substring(0, Math.min(100, description.length))}</span>
                 {description.length > 100 && <span className="text-pnw-gold opacity-70 hover:cursor-pointer hover:opacity-100" onClick={() => setShow(true)}> (Expand)</span>}
--- app/project-spotlight/page.tsx
+++ app/project-spotlight/page.tsx
@@ -13,14 +13,26 @@ export default function Page() {
                 Wondering what 3D Printing could be used for? Learn about the projects our members have done!
             </p>
         </HorizontalWrap>
-        <div className="bg-white h-screen">
+        <div className="bg-white">
             <HorizontalWrap>
-                <div className="grid lg:grid-cols-2 2xl:grid-cols-3 gap-12 mt-8">
+                <div className="grid lg:grid-cols-2 2xl:grid-cols-3 gap-8 py-8">
                     <Project
                         title="Reviving the Spotify Car Thing"
                         author="Aaron Jung"
                         imageSrc="/assets/car_thing.jpg"
-                        description="I bought a Spotify Car Thing in 2022 and used it regularly until it was discontinued. I decided to repurpose it using DeskThing and designed a custom 3D Printed mount for my monitor to keep it easily accessible on my desktop." />
+                        description="Aaron bought a Spotify Car Thing in 2022 and used it regularly until it was discontinued. He decided to repurpose it using DeskThing and designed a custom 3D Printed mount for his monitor to keep it easily accessible." />
+
+                    <Project
+                        title="Building a Battle Bot"
+                        author="Luke Moreno"
+                        imageSrc="/assets/luke_moreno_battle_bot_2.jpg"
+                        description="Luke uses ABS & TPU filament to encase the electronics of his three-pound Battle Bot. This durable material offers excellent impact resistance. It helps soften blows from opposing weapons during battles." />
+
+                    <Project
+                        title="IEEE Welcome Rally Tags"
+                        author="Anhviet Le"
+                        imageSrc="/assets/ieee_tags.jpg"
+                        description="Le leverages 3D Printing to craft custom tags for the IEEE chapter at PNW for the Welcome Rally. You've probably seen these around campus on backpacks!" />
 
                     <Project
                         title="Making an Electric Stakeboard"
