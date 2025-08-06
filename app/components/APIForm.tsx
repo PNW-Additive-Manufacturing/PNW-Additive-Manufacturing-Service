3ebbc273c89f54438644854021025abea49eccf4
Aaron J <aaronjung@me.com>
2025-08-06 15:11:22 -0500

    refactor APIForm to improve error handling and add support for custom success messages

--- app/components/APIForm.tsx
+++ app/components/APIForm.tsx
@@ -1,29 +1,39 @@
-import { useMemo, useState } from "react";
-import { APIData } from "../api/APIResponse";
+import { useCallback, useMemo, useState } from "react";
+import { APIData, resError } from "../api/APIResponse";
 import FormSubmitButton from "./FormSubmitButton";
 
 type APIFormProps<T> = React.PropsWithChildren<{
     action: (formData: FormData) => Promise<APIData<T>>,
-    onRes?: (result: APIData<T>) => void,
-    submitLabel?: string
+    onRes?: (result: APIData<T>) => void, // Generic on response handler.
+    successMessage?: (data: T) => JSX.Element, // Easily display custom success messages.
+    submitLabel?: string // Label for the submit button - no button will be auto-included if not provided.
 }>
 
-export default function APIForm<T>({ children, action, onRes, submitLabel }: APIFormProps<T>) {
-    const [result, setResults] = useState<APIData<T> | undefined>(undefined);
+export default function APIForm<T>({ children, action, onRes, submitLabel, successMessage }: APIFormProps<T>) {
+    const [result, setResult] = useState<APIData<T> | undefined>(undefined);
 
-    // TODO: Set to a memo.
-    async function submitAPI(formData: FormData) {
-        const givenResult = await action(formData);
-        setResults(givenResult);
+    const submitAPI = useCallback(async (formData: FormData) => {
 
-        if (onRes !== undefined) onRes(givenResult);
-    }
+        try {
+            const apiResult = await action(formData);
+
+            setResult(apiResult);
+            onRes?.(apiResult);
+        }
+        catch (error) {
+            console.error(error);
+            setResult(resError("An internal error occurred! Check dev-tools/inspect."));
+        }
+
+    }, [action, onRes]);
 
     return <form action={submitAPI}>
 
         {children}
 
-        {result && !result.success && <p>An issue occurred{result.errorMessage !== undefined && `: ${result.errorMessage}`}</p>}
+        {result && (result.success
+            ? (successMessage && <p className="text-sm mb-4">{successMessage(result)}</p>)
+            : <p className="text-warning text-sm mb-4">{result?.errorMessage === undefined ? "An unknown issue has occurred!" : `An issue has occurred: ${result?.errorMessage}`}</p>)}
 
         {submitLabel && <FormSubmitButton label={submitLabel} />}
 
