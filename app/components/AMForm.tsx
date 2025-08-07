317552123ee4dfd130b999f8cd970bd6cbbb7851
Aaron J <aaronjung@me.com>
2025-08-06 20:32:34 -0500

    add AMForm component to standardize UI login, forgot-password, and etc.

---
+++ app/components/AMForm.tsx
@@ -0,0 +1,37 @@
+import AMSIcon from "./AMSIcon";
+import DragInOnView from "./animation/DragOnView";
+
+type AMFormProps = React.PropsWithChildren<{
+    title: React.JSX.Element | string,
+    description?: React.JSX.Element | string;
+}>;
+
+export default function AMForm({ children, title, description }: AMFormProps) {
+    return <>
+
+        <DragInOnView direction={"up"}>
+
+            <div className="flex flex-col gap-4">
+
+                <div className="w-fit lg:mx-auto">
+                    <AMSIcon />
+                </div>
+
+                <h1 className="text-xl mx-auto font-normal w-fit">
+                    {title}
+                </h1>
+
+                <hr className="m-3" />
+
+                {description && <p className="text-sm">{description}</p>}
+
+                <div className="flex flex-col gap-4">
+                    {children}
+                </div>
+
+            </div>
+
+        </DragInOnView>
+
+    </>
+}
