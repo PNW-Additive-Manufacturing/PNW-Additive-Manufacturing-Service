b88e35cd70f865443a4939bec0e231f557cd6a33
Aaron J <aaronjung@me.com>
2025-08-27 11:14:20 -0500

    refine opengraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,17 +30,19 @@ export async function generateMetadata(
 	return Promise.resolve({
 		metadataBase: new URL(envConfig.hostURL),
 		title: "PNW Additive Manufacturing Service",
-		description:
-			"Created by the PNW Additive Manufacturing Club, this service enables PNW students, and faculty members to explore the world of 3D Printing.",
+		description: "Providing PNW students and faculty with access to advanced 3D Printing technology.",
 		icons: ["/assets/am_logo_light.png"],
 		applicationName: "PNW Additive Manufacturing Service",
 		category: "3D Printing",
+		themeColor: "#f5b626",
 		openGraph: {
+			url: envConfig.hostURL,
+			siteName: "PNW Additive Manufacturing Club",
 			type: "website",
-			title: "PNW Additive Manufacturing Service",
-			images: [`${envConfig.hostURL}/assets/am_logo_light.png`],
-			description: "Created by the PNW Additive Manufacturing Club, this service enables PNW students, and faculty members to explore the world of 3D Printing.",
-			locale: "en_US"
+			title: "PNW Students: 3D Print Your Designs at PNW",
+			description: "Providing PNW students and faculty with access to advanced 3D Printing technology.",
+			locale: "en_US",
+			images: []
 		}
 	});
 }
@@ -72,7 +74,6 @@ export default async function RootLayout({
 					href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
 					rel="stylesheet"
 				/>
-				<meta name="theme-color" content="#b1810b" />
 				<meta
 					name="viewport"
 					content="width=device-width, initial-scale=1.0"></meta>
